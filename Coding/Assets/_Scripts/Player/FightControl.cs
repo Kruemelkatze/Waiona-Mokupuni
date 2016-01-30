fa4be8325f73576dace344e5dc2a33e92bb11bf0
Alexander Kopper <code@alexander-kopper.at>
2016-01-30 18:34:59 +0100

    Chrods playing

--- Coding/Assets/_Scripts/Player/FightControl.cs
+++ Coding/Assets/_Scripts/Player/FightControl.cs
@@ -22,8 +22,35 @@ public class FightControl : MonoBehaviour {
 	
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame 	
 	void Update () {
-	
+		if(Input.GetButtonDown("Fire1")) {
+			PlayChord1 ();
+		}
+		if(Input.GetButtonDown("Fire2")) {
+			PlayChord2 ();
+		}
+		if(Input.GetButtonDown("Fire3")) {
+			PlayChord3 ();
+		}
+		if(Input.GetButtonDown("Jump")) {
+			PlayChord4 ();
+		}
 	}
+
+	private void PlayChord1() {
+		Grid.SoundManager.PlaySingle (Chord1);
+	}
+	private void PlayChord2() {
+		Grid.SoundManager.PlaySingle (Chord2);
+	}
+	private void PlayChord3() {
+		Grid.SoundManager.PlaySingle (Chord3);
+	}
+	private void PlayChord4() {
+		Grid.SoundManager.PlaySingle (Chord4);
+	}
+
+
+
 }
