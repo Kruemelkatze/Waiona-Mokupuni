9e2cb3ce552127fcefd23b6dd126c4a52ee92c8e
Fabian <schoberf@gmx.net>
2016-01-30 18:22:00 +0100

    Foreign Altars

---
+++ Coding/Assets/_Scripts/ForeignAltar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForeignAltar : MonoBehaviour {
+
+	private GameObject elementPickup;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform) {
+			if(child.gameObject.tag.Equals("ElementPickup")) {
+				elementPickup = child.gameObject;
+				break;
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other != Grid.Player) {
+			return;
+		}
+		Grid.EventHub.TriggerRunOverElement (elementPickup);
+	}
+}
