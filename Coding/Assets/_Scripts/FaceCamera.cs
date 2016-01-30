d8dccf20b77422e9ad24e141e5eb2254c666104e
Fabian <schoberf@gmx.net>
2016-01-30 18:32:06 +0100

    ElementPickups face camera // Pickup Event

---
+++ Coding/Assets/_Scripts/FaceCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.LookAt (Camera.main.transform.position, -Vector3.up);
+	}
+}
