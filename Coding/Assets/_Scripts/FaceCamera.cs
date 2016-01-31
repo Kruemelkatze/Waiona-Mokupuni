ede806552b02c72118b5fc6c3e9d33dda03a6794
Fabian <schoberf@gmx.net>
2016-01-31 10:44:31 +0100

    Fixed FaceCamera Script for new camera positioning

--- Coding/Assets/_Scripts/FaceCamera.cs
+++ Coding/Assets/_Scripts/FaceCamera.cs
@@ -5,6 +5,6 @@ public class FaceCamera : MonoBehaviour {
 
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.LookAt (Camera.main.transform.position, -Vector3.up);
+		transform.LookAt (Camera.main.transform.position, Vector3.up);
 	}
 }
