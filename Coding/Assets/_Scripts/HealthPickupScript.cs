63b90a98d1db38eebff75a5a64766d65e05e3758
Fabian <schoberf@gmx.net>
2016-01-31 11:25:37 +0100

    HealthPickups and sound

---
+++ Coding/Assets/_Scripts/HealthPickupScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickupScript : MonoBehaviour {
+	public AudioClip PickupSound;
+	public int healthValue = 1;
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject != Grid.Player || (healthValue > 0 && Grid.GameLogic.CurrentLife == Grid.GameLogic.MaxLife)) {
+			return;
+		}
+
+		Grid.EventHub.TriggerLifeChanged (healthValue);
+		Grid.SoundManager.PlaySingle (PickupSound, 1);
+		Destroy (gameObject);
+	}
+}
